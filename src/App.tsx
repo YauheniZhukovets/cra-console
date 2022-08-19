5fb5128b2a3f62fc6f62f933ad1c8df013f2e3d7
zhukavets <stewart-8@mail.ru>
2022-08-19 12:38:19 +0300

    Dev branch commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>App</div>
-  );
+    return (
+        <div>
+            <div>App</div>
+            <div>Dev branch commit - 1</div>
+        </div>
+    );
 }
 
 export default App;
